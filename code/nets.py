694918f90a066895a3e7daf85795dcab1d52e8b1
Sihao <1040296932@qq.com>
2017-12-13 19:28:16 +0800

    finish network structure

--- code/nets.py
+++ code/nets.py
@@ -1,7 +1,12 @@
+import tensorflow as tf
+
 n_input = 28 * 28
-n_l1 = 512
-n_l2 = 256
-n_l3 = 128
+n_l1 = 256
+n_l2 = 128
+n_l3 = 64
+n_l4 = 26
+skip_reg = 0.00005
+vanilla_reg = 0.0005
 
 class skip_network:
     def __init__(self, scope, lr):
@@ -11,30 +16,43 @@ class skip_network:
             self.n_l1 = n_l1
             self.n_l2 = n_l2
             self.n_l3 = n_l3
+            self.n_l4 = n_l4
+            self.reg = skip_reg
 
-            self.input = tf.placeholder(tf.float32, [None, n_input], name='input')
+            self.raw_input = tf.placeholder(tf.float32, [None, 28, 28], name='input')
+            self.input = tf.reshape(self.raw_input, [-1, 784])
 
             self.W1 = tf.get_variable('W1', [self.n_input, self.n_l1], initializer=tf.contrib.layers.xavier_initializer())
             self.W2 = tf.get_variable('W2', [self.n_l1, self.n_l2], initializer=tf.contrib.layers.xavier_initializer())
             self.W3 = tf.get_variable('W3', [self.n_l2, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
-            self.W4 = tf.get_variable('W4', [self.n_input, self.n_l2], initializer=tf.contrib.layers.xavier_initializer())
-            self.W5 = tf.get_variable('W5', [self.n_input, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
-            self.W6 = tf.get_variable('W6', [self.n_l1, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
+            self.W4 = tf.get_variable('W4', [self.n_l3, self.n_l4], initializer=tf.contrib.layers.xavier_initializer())
+            self.W0_2 = tf.get_variable('W0_2', [self.n_input, self.n_l2], initializer=tf.contrib.layers.xavier_initializer())
+            self.W1_3 = tf.get_variable('W1_3', [self.n_l1, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
+            self.W0_3 = tf.get_variable('W0_3', [self.n_input, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
+            self.W2_4 = tf.get_variable('W2_4', [self.n_l2, self.n_l4], initializer=tf.contrib.layers.xavier_initializer())
+            self.W1_4 = tf.get_variable('W1_4', [self.n_l1, self.n_l4], initializer=tf.contrib.layers.xavier_initializer())
+            self.W0_4 = tf.get_variable('W0_4', [self.n_input, self.n_l4], initializer=tf.contrib.layers.xavier_initializer())
             self.b1 = tf.get_variable('b1', [self.n_l1], initializer=tf.constant_initializer(0.01))
             self.b2 = tf.get_variable('b2', [self.n_l2], initializer=tf.constant_initializer(0.01))
-            self.b3 = tf.get_variable('b3', [self.n_l3], initializer=tf.constant_initializer(0.00))
+            self.b3 = tf.get_variable('b3', [self.n_l3], initializer=tf.constant_initializer(0.01))
+            self.b4 = tf.get_variable('b4', [self.n_l4], initializer=tf.constant_initializer(0.00))
 
             self.layer1 = tf.nn.relu(tf.matmul(self.input, self.W1) + self.b1)
-            self.layer2 = tf.nn.relu(tf.matmul(self.layer1, self.W2) + tf.matmul(self.input, self,W4) + self.b2)
-            self.layer3 = tf.matmul(self.layer2, self.W3) + tf.matmul(self.layer1, self.W5) + tf.matmul(self.input, self.W6) + self.b3
+            self.layer2 = tf.nn.relu(tf.matmul(self.layer1, self.W2) + tf.matmul(self.input, self.W0_2) + self.b2)
+            self.layer3 = tf.nn.relu(tf.matmul(self.layer2, self.W3) + tf.matmul(self.layer1, self.W1_3) + tf.matmul(self.input, self.W0_3) + self.b3)
+            self.layer4 = tf.matmul(self.layer3, self.W4) + tf.matmul(self.layer2, self.W2_4) + tf.matmul(self.layer1, self.W1_4) + tf.matmul(self.input, self.W0_4) + self.b4
 
-            self.predictions = tf.argmax(self.layer3, axis=1) + 1
+            self.predictions = tf.argmax(self.layer4, axis=1) + 1
 
             self.labels = tf.placeholder(tf.int32, [None], name='labels')
-            self.labels = self.labels - 1
-            self.labels_onehot = tf.one_hot(indices=self.label, depth=26)
+            self.labels_modified = self.labels - 1
+            self.labels_onehot = tf.one_hot(indices=self.labels_modified, depth=26)
 
-            self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(self.labels_onehot, self.layer3))
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_onehot, logits=self.layer4)) + \
+                        self.reg * (tf.reduce_sum(self.W1 * self.W1) + tf.reduce_sum(self.W2 * self.W2) + tf.reduce_sum(self.W3 * self.W3) + tf.reduce_sum(self.W4 * self.W4) + \
+                        tf.reduce_sum(self.W0_2 * self.W0_2) + \
+                        tf.reduce_sum(self.W1_3 * self.W1_3) + tf.reduce_sum(self.W0_3 * self.W0_3) + \
+                        tf.reduce_sum(self.W2_4 * self.W2_4) + tf.reduce_sum(self.W1_4 * self.W1_4) + tf.reduce_sum(self.W0_4 * self.W0_4))
 
             self.optimize = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.loss)
 
@@ -46,26 +64,33 @@ class vanilla_network:
             self.n_l1 = n_l1
             self.n_l2 = n_l2
             self.n_l3 = n_l3
+            self.n_l4 = n_l4
+            self.reg = vanilla_reg
 
-            self.input = tf.placeholder(tf.float32, [None, n_input], name='input')
+            self.raw_input = tf.placeholder(tf.float32, [None, 28, 28], name='input')
+            self.input = tf.reshape(self.raw_input, [-1, 784])
 
             self.W1 = tf.get_variable('W1', [self.n_input, self.n_l1], initializer=tf.contrib.layers.xavier_initializer())
             self.W2 = tf.get_variable('W2', [self.n_l1, self.n_l2], initializer=tf.contrib.layers.xavier_initializer())
             self.W3 = tf.get_variable('W3', [self.n_l2, self.n_l3], initializer=tf.contrib.layers.xavier_initializer())
+            self.W4 = tf.get_variable('W4', [self.n_l3, self.n_l4], initializer=tf.contrib.layers.xavier_initializer())
             self.b1 = tf.get_variable('b1', [self.n_l1], initializer=tf.constant_initializer(0.01))
             self.b2 = tf.get_variable('b2', [self.n_l2], initializer=tf.constant_initializer(0.01))
-            self.b3 = tf.get_variable('b3', [self.n_l3], initializer=tf.constant_initializer(0.00))
+            self.b3 = tf.get_variable('b3', [self.n_l3], initializer=tf.constant_initializer(0.01))
+            self.b4 = tf.get_variable('b4', [self.n_l4], initializer=tf.constant_initializer(0.00))
 
             self.layer1 = tf.nn.relu(tf.matmul(self.input, self.W1) + self.b1)
             self.layer2 = tf.nn.relu(tf.matmul(self.layer1, self.W2) + self.b2)
-            self.layer3 = tf.matmul(self.layer2, self.W3) + self.b3
+            self.layer3 = tf.nn.relu(tf.matmul(self.layer2, self.W3) + self.b3)
+            self.layer4 = tf.matmul(self.layer3, self.W4) + self.b4
 
-            self.predictions = tf.argmax(self.layer3, axis=1) + 1
+            self.predictions = tf.argmax(self.layer4, axis=1) + 1
 
             self.labels = tf.placeholder(tf.int32, [None], name='labels')
-            self.labels = self.labels - 1
-            self.labels_onehot = tf.one_hot(indices=self.label, depth=26)
+            self.labels_modified = self.labels - 1
+            self.labels_onehot = tf.one_hot(indices=self.labels_modified, depth=26)
 
-            self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(self.labels_onehot, self.layer3))
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.labels_onehot, logits=self.layer4)) + \
+                        self.reg * (tf.reduce_sum(self.W1 * self.W1) + tf.reduce_sum(self.W2 * self.W2) + tf.reduce_sum(self.W3 * self.W3) + tf.reduce_sum(self.W4 * self.W4))
 
             self.optimize = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.loss)
